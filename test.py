b034589d8aa1bc2d223b39132981611b57c9adfc
Archimedes <archimedessena@gmail.com>
2020-09-11 22:14:03 +0000

    algo updated

--- test.py
+++ test.py
@@ -1,17 +1,37 @@
-# Testing recursion 
-def countdown(i):
-    if i <= 0: # Base case
-        return i
-    else: # Recursive Base
-        countdown(i-1)
+# test field
+class Binary:
+    def binary_search(self, list, item):
+        lowest = 0
+        highest = len(list) - 1
+        while lowest <= highest:
+            midval = (lowest + highest)
+            suggest = list[midval]
+            if suggest == item:
+                return midval
+            if suggest > item:
+                highest = midval - 1
+            else:
+                lowest = midval + 1 
+        return None
 
 
 
-def fact(x):
-    if x == 1:
-        return 1
-    else: 
-        return 2 + fact(x - 1)
+        def smallest_number(self, arr):
+            lowest_number = 0
+            position = arr[0]
+            for i in range(1, len(arr)):
+                if arr[i] < position:
+                    lowest_number = arr[i]
+                    position = i
+                return position
 
-    
-print(fact(3))
+
+
+
+calls = Binary()
+nice = calls.binary_search([4, 67, 78, 90], 90)
+print(nice)
+
+sorted = Binary()
+sorting = sorted.smallest_number(nice)
+print(sorting)
